7dcd82052e2b4aa10015e90d58a5332b004f59c0
davidabadi <david@abadi.org>
2017-11-13 21:30:04 -0500

    Revert "Test commit"
    This reverts commit 965837f5e4957e73df603f52d71f67367591193a.

--- MyApplication/app/src/main/java/cheapskate/helloworld/MainActivity.java
+++ MyApplication/app/src/main/java/cheapskate/helloworld/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
-//test commit
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
